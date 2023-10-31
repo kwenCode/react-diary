2e218c6a9458742f50ea646b1b0502f52fe689b1
kwanhyo <lhc2472@gmail.com>
2023-10-31 22:56:02 +0900

    EmotionItem add

--- src/util.js
+++ src/util.js
@@ -21,3 +21,44 @@ export const getEmotionImgByid = (emotionId) => {
             return null;
     }
 };
+
+export const getFormattedDate = (targetDate) => {
+    let year = targetDate.getFullYear();
+    let month = targetDate.getMonth() + 1;
+    let date = targetDate.getDate();
+    if(month < 10 ){
+        month = `0${month}`;
+    }
+    if(date < 10 ){
+        date = `0${date}`;
+    }
+    return `${year}-${month}-${date}`;
+}
+
+export const emotionList = [
+    {
+        id: 1,
+        name: "완전 좋음",
+        img: getEmotionImgByid(1),
+    },
+    {
+        id: 2,
+        name: "좋음",
+        img: getEmotionImgByid(2),
+    },
+    {
+        id: 3,
+        name: "그럭저럭",
+        img: getEmotionImgByid(3),
+    },
+    {
+        id: 4,
+        name: "나쁨",
+        img: getEmotionImgByid(4),
+    },
+    {
+        id: 5,
+        name: "끔찍함",
+        img: getEmotionImgByid(5),
+    },
+]
